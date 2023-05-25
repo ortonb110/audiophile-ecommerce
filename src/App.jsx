9df79ff6d665527beca95b99a6dfbcff0332659b
Orton Bright <ortonb110@gmail.com>
2023-05-25 19:56:54 +0000

    Basic Setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <h1>Morbi interdum mollis sapien</h1>
+      <h2>Donec nec justo eget felis facilisis</h2>
+      <h3>Nunc sem lacus accum</h3>
+      <h4>interdum consectetuer</h4>
+      <h5>nascetur ridiculus musnascetur ridiculus mus</h5>
+      <h6>natoque penatibus et</h6>
+      <p className="overLine">bright orton</p>
+      <p className="subTitle">mavis ofori</p>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+         "orange-dark": "#D87D4A",
+          "orange-light": "#fbaf85",
+          "black-light": "#101010",
+          "gray-light": "#f1f1f1",
+          "gray-veryLight": "#fafafa",
+
+      },
+    },
   },
   plugins: [],
 }
